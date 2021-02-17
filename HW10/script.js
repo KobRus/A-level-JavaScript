4af9a0d5cd3f2bdccd71fb51eb4930711435b3dd
Ruslan <ruslankobozev@gmail.com>
2021-02-17 14:06:03 +0200

    HW10

---
+++ HW10/script.js
@@ -0,0 +1,53 @@
+const section = document.createElement('div')
+section.id = 'section'
+document.body.prepend(section)
+
+section.style.display = 'flex'
+section.style.flexWrap = 'wrap'
+
+const promise =  fetch('https://api.punkapi.com/v2/beers?brewed_before=11-2017&abv_gt=6')
+
+promise
+    .then((response) => {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem');
+            console.log(`Status Code: ${response.status}`);
+            return;
+        }
+        return response.json();
+    }
+    )
+
+    .then((data) => {
+        data.forEach((item) => {
+            const imageContainer = document.createElement('div')
+            const textContainer = document.createElement('div')
+            const img = document.createElement('img')
+            img.setAttribute('src', item.image_url) 
+            imageContainer.appendChild(img)
+
+            img.style.width = '100px'
+            img.style.margin = '20px'
+
+            textContainer.innerHTML = item.description
+
+            section.appendChild(imageContainer)
+            section.appendChild(textContainer)
+
+            imageContainer.style.width = '40%'
+            imageContainer.style.paddingLeft = '100px'
+
+            textContainer.style.width = '50%'
+            textContainer.style.alignItems = 'center'
+            textContainer.style.display = 'flex'
+
+            img.addEventListener('click', () => {
+                let bearId = JSON.stringify(item.id)
+                localStorage.setItem('key', bearId)
+            })
+        }); 
+    })
+
+        .catch((err) => {
+            console.log('Fetch Error', err);
+        })
